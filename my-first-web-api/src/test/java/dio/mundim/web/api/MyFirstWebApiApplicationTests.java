24ad8701a693055b34652c6957b511d4696c1cdc
BrunoMundim <brunomundimfranco@gmail.com>
2022-10-30 03:51:54 -0300

    Iniciando configurações

---
+++ my-first-web-api/src/test/java/dio/mundim/web/api/MyFirstWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.mundim.web.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
